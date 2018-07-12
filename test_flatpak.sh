45da3d5667089fd764714768da766c58a001cafb
Alexey Minnekhanov <a.minnekhanov@omprussia.ru>
2018-07-12 11:48:25 +0300

    Add script to build flatpak and test run

---
+++ test_flatpak.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+rm -rf ./.flatpak-builder
+rm -rf ./.flatpak-builddir
+flatpak-builder flatpak-builddir ru.minlexx.qqc2hello.json
+flatpak-builder --run flatpak-builddir ru.minlexx.qqc2hello.json qqc2hello
